e79e09a2efd129748fb5e08531a104e33431cd19
Alexander Doroschuk <btw210899@gmail.com>
2020-12-23 23:31:23 +0200

    release v1.0

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import './spinner.css';
+
+const Spinner = () => {
+  return (
+    <div className="loadingio-spinner-double-ring-60sim73fvkm">
+      <div className="ldio-xuihq0jbt9o">
+        <div></div>
+        <div></div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default Spinner;
